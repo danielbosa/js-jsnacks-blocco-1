11c9381eed1d9e52e9b1adeb617a64c0ee3e8f8b
danielbosa <danielbosa95@gmail.com>
2024-03-12 16:26:46 +0100

    JS Error fixed. Bonus accomplished

--- array_tail/script.js
+++ array_tail/script.js
@@ -11,23 +11,28 @@ Probabilmente conviene controllare che il numero di elementi della coda non sia 
 1. Chiedere a utente quanti elementi, tramite prompt;
 2. Dichiarare array vuoto;
 3. Generare n elementi con ciclo for, dove n = numero dell'utente; ciclo for deve girare da i = 0 a i <= n;
-4. Ad ogni ciclata, aggiungo elemento n(i) tramite push all'array vuoto;
+4. Ad ogni ciclata, aggiungo elemento n(i) valorizzando direttamente l'elemento nell'array;
 5. Stampo in console.log gli ultimi 5 elementi dell'array: array[array.lenght-5...-1] --> ciclo for al contrario.
     ciclo da i = 5 a i = 1 compreso (i = 5; i >= 1; i--) e ad ogni ciclata stampo l'elemento selezionato dall'array;
 BONUS
 
 */
 
-let numElementi = parseInt(prompt('Quanti elementi vuoi vedere?'));
+let numElementi = parseInt(prompt('Quanti elementi vuoi?'));
 let arrayVuoto = [];
 
 for (i = 0; i < numElementi; i++){
-    arrayVuoto[i] = RndNumberGen(1, 100);
-    console.log(arrayVuoto[i]);
-    arrayVuoto.push(arrayVuoto[i]);
-    console.log(arrayVuoto);
+//console.log(arrayVuoto);
 };
 
-for(i = 5; i >= 1; i--){
-    console.log(arrayVuoto[arrayVuoto.length - i]);
-};
+let numVisibili = parseInt(prompt('Quanti elementi vuoi vedere, a partire dal fondo dell\'elenco?'));
+
+if(numVisibili > arrayVuoto.length){
+    console.log('Mi sembra una richiesta esagerata... Non ci sono abbastanza elementi!')
+} else {
+    for(i = numVisibili; i >= 1; i--){
+        console.log(arrayVuoto[arrayVuoto.length - i]);
+    };
+}
+
+
