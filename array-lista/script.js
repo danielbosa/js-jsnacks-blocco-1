4a70a3051e24689bab481b0ea24e5044dc3ad75a
danielbosa <danielbosa95@gmail.com>
2024-03-12 15:34:29 +0100

    Array-list: completed

---
+++ array-lista/script.js
@@ -0,0 +1,34 @@
+/*
+Abbiamo un frigorifero pieno di frutta:
+'banana', 'mela', 'pera', 'ciliegia', 'arancia', 'mandarino', 'cocomero', 'limone', 'fragola'  
+C'è anche una pesca sul tavolo, la mettiamo nel frigo.
+Stasera dobbiamo fare un cocktail a base di cocomero: ce l'abbiamo o no in frigo?
+Fasi
+1. creare l'array con la frutta del frigorifero
+2. aggiungere la pesca all'array della frutta
+3. verificare se nell'array di frutta c'è il cocomero:
+   - se c'è stampiamo: "Trovato! Devo solo preparare il cocktail."
+   - se non lo trovo: "Oh no, devo uscire a comprare il cocomero!"
+*/
+
+let frigorifero = ['banana', 'mela', 'pera', 'ciliegia', 'arancia', 'mandarino', 'cocomero', 'limone', 'fragola'];
+let itemToAdd = 'pesca';
+let itemToCheck = 'cocomero';
+
+//console.log(frigorifero);
+
+frigorifero.push(itemToAdd);
+//console.log(frigorifero);
+
+let check = false;
+
+for (i = 0; i < frigorifero.length; i++){
+    if (frigorifero[i] == itemToCheck){
+        check = true;
+    }
+    //console.log(frigorifero[i]);
+    //console.log(itemToCheck);
+    //console.log(check);
+};
+
+(check === true)? console.log(`Trovato! Devo solo preparare il cocktail.`) : console.log(`Oh no, devo uscire a comprare il cocomero!`);
